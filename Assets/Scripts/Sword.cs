9c633a80396911978082e843e49f3389c39f100e
Katari Yuit <katariyuit.company@gmail.com>
2023-05-22 09:59:01 +0700

    fixer for sword
    On Sword Class:
    - create variable: playerController, activeWeapon
    - creater funtion: MouseFollowWithOffset
    + get mouse posion and player position on screen
    + compare 2 variabe and rotation ActiveWeapon object
    add ActiveWeapon Script on ActiveWeapon Object
    add comment

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator swordAnim;
-
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     /// <summary>
     /// Unity System method
     /// </summary>
     private void Awake()
     {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         swordAnim = GetComponent<Animator>();
         playerControls = new PlayerControls();
     }
@@ -22,16 +26,41 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
     /// <summary>
     /// Sword Method
     /// </summary>
+    // enable for PlayerControls Script on class
     private void OnEnable()
     {
         playerControls.Enable();
     }
 
+    // get trigger creat motion for sword
     private void Attack()
     {
         swordAnim.SetTrigger("Attack");
+    }
+
+    // sword has facing to mouse
+    private void MouseFollowWithOffset()
+    {
+        // get mouse position in screen
+        Vector3 mousePos = Input.mousePosition;
+        // get player position in screen 
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        // check location for player facing
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 180, 0);
+        } else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
